b75d635e7a93d9146c3a035b2912763ff20523ca
Zeljko-Cenger <zac1008.zc@gmail.com>
2024-10-01 20:15:44 +0200

    End of section 15

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using AutoMapper;
 using Core.Entities;
+using Core.Entities.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Helpers
@@ -17,6 +18,8 @@ namespace API.Helpers
                 .ForMember(d=>d.ProductBrand, o=>o.MapFrom(s=>s.ProductBrand.Name))
                 .ForMember(d=>d.ProductType, o=>o.MapFrom(s=>s.ProductType.Name))
                 .ForMember(d=>d.PictureUrl, o=>o.MapFrom<ProductUrlResolver>());
+
+            CreateMap<Address, AddressDto>().ReverseMap();   
                 
         }
     }
